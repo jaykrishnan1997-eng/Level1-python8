792a9c8817a349718f9b56a0c98cfa9d8cfc9682
Jay Krishnan <jkrishna@student.42heilbronn.de>
2026-07-17 14:33:56 +0200

    ex1 half

--- ex1/loading.py
+++ ex1/loading.py
@@ -7,7 +7,7 @@
 #   By: jkrishna <jkrishna@student.42.fr>            +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/07/07 15:22:12 by jkrishna            #+#    #+#            #
-#   Updated: 2026/07/15 14:09:44 by jkrishna           ###   ########.fr      #
+#   Updated: 2026/07/17 14:32:05 by jkrishna           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
@@ -19,30 +19,64 @@ def check_package(name):
     return spec is not None
 
 
-def check_dependencies():
+def check_dependencies() -> bool:
     packages = {
         "pandas": "Data manipulation",
         "numpy": "Numerical computation",
-        "requests": "Network access",
         "matplotlib": "Visualization",
+        "requests": "Network access"
     }
     status = {}
+    versions = {}
+
     for name in packages.keys():
         if check_package(name):
-            module = importlib.import_module(name)
-            version = module.__version__
-            status[name] = "OK"
+            try:
+                module = importlib.import_module(name)
+                versions[name] = module.__version__
+                status[name] = "OK"
+            except Exception:
+                status[name] = "KO"
+                versions[name] = None
+                print(f"[KO] {name} found but failed to import: {e}")
         else:
             status[name] = "KO"
 
     for name in packages.keys():
         if status[name] == "OK":
-            print(f"[OK] {name} {version} {packages[name]} ready")
+            print(f"[OK] {name} ({versions[name]}) - {packages[name]} ready")
+        elif check_package(name):
+            continue
         else:
             print(f"[KO] {name} is not installed")
+            print(f"To install, use : 'pip install {name}'")
+    if "KO" in status:
+        return False
+    return True
+
+
+def generating_vis():
+    import requests
+
+    URL = "https://opendata.cern.ch/api/records/?size=100"
+
+    response = requests.get(URL, timeout=10)
+    response.raise_for_status()
+
+    records = response.json()["hits"]["hits"]
+    titles = []
+    experiments = []
+    
+    for record in records:
+        
+
+
+def main():
+    print("LOADING STATUS: Loading programs...\n")
+    print("Checking dependencies:")
+    if check_dependencies() is False:
+        exit
 
 
 if __name__ == "__main__":
-    print("LOADING STATUS: Loading programs...\n")
-    print("Checking dependencies:")
-    check_dependencies()
+    main()
